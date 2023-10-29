db6ce0db3fe64f2bbc3b473fbee779c2b03df7ca
akaiDing <tingtind@umich.edu>
2023-10-29 15:01:14 -0400

    add uploadusdzview

---
+++ SpaceBlender/SpaceBlender/UploadUsdzView.swift
@@ -0,0 +1,16 @@
+//
+//  UploadUsdzView.swift
+//  SpaceBlender
+//
+//  Created by akai on 10/29/23.
+//
+
+import SwiftUI
+import RoomPlan
+
+struct UploadUsdzView: View {
+    
+    var body: some View {
+        Text("todo: scanning view")
+    }
+}

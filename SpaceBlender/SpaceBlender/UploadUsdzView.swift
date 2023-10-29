d62f159e24cd12a02160179a5df94781dcbb2380
akaiDing <tingtind@umich.edu>
2023-10-29 15:17:31 -0400

    try some upload

--- SpaceBlender/SpaceBlender/UploadUsdzView.swift
+++ SpaceBlender/SpaceBlender/UploadUsdzView.swift
@@ -8,9 +8,55 @@
 import SwiftUI
 import RoomPlan
 
+import UIKit
+import MobileCoreServices
+
+struct DocumentPicker: UIViewControllerRepresentable {
+    @Binding var fileURL: URL?
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+
+    func makeUIViewController(context: UIViewControllerRepresentableContext<DocumentPicker>) -> UIDocumentPickerViewController {
+        let documentPicker = UIDocumentPickerViewController(documentTypes: [String(kUTTypeText)], in: .import)
+        documentPicker.allowsMultipleSelection = false
+        documentPicker.delegate = context.coordinator
+        return documentPicker
+    }
+
+    func updateUIViewController(_ uiViewController: UIDocumentPickerViewController, context: UIViewControllerRepresentableContext<DocumentPicker>) {}
+
+    class Coordinator: NSObject, UIDocumentPickerDelegate {
+        var parent: DocumentPicker
+
+        init(_ pickerController: DocumentPicker) {
+            self.parent = pickerController
+        }
+
+        func documentPicker(_ controller: UIDocumentPickerViewController, didPickDocumentsAt urls: [URL]) {
+            guard let url = urls.first else { return }
+            parent.fileURL = url
+        }
+
+        func documentPickerWasCancelled(_ controller: UIDocumentPickerViewController) {
+            controller.dismiss(animated: true)
+        }
+    }
+}
+
 struct UploadUsdzView: View {
+    @State private var fileURL: URL?
+    @State private var showDocumentPicker = false
     
     var body: some View {
-        Text("todo: scanning view")
+        VStack {
+            Button("Open Document Picker") {
+                showDocumentPicker = true
+            }
+        }
+        .sheet(isPresented: $showDocumentPicker) {
+            DocumentPicker(fileURL: $fileURL)
+        }
     }
 }

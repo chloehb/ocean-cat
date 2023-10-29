7a0cfc6a6ea08bf932a8b0d1c199dece442e7d93
zyliang <zyliang@umich.edu>
2023-10-29 15:38:23 -0400

    Usdz

--- SpaceBlender/SpaceBlender/UploadUsdzView.swift
+++ SpaceBlender/SpaceBlender/UploadUsdzView.swift
@@ -8,6 +8,46 @@
 import SwiftUI
 import RoomPlan
 
+import UIKit
+import MobileCoreServices
+
+import UniformTypeIdentifiers
+
+struct DocumentPicker: UIViewControllerRepresentable {
+    @Binding var fileURL: URL?
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+
+    func makeUIViewController(context: UIViewControllerRepresentableContext<DocumentPicker>) -> UIDocumentPickerViewController {
+//        let documentPicker = UIDocumentPickerViewController(documentTypes: [String(kUTTypeText)], in: .import)
+        let documentPicker = UIDocumentPickerViewController(forOpeningContentTypes: [UTType.usdz], asCopy: true)
+        documentPicker.allowsMultipleSelection = false
+        documentPicker.delegate = context.coordinator
+        return documentPicker
+    }
+
+    func updateUIViewController(_ uiViewController: UIDocumentPickerViewController, context: UIViewControllerRepresentableContext<DocumentPicker>) {}
+
+    class Coordinator: NSObject, UIDocumentPickerDelegate {
+        var parent: DocumentPicker
+
+        init(_ pickerController: DocumentPicker) {
+            self.parent = pickerController
+        }
+
+        func documentPicker(_ controller: UIDocumentPickerViewController, didPickDocumentsAt urls: [URL]) {
+            guard let url = urls.first else { return }
+            parent.fileURL = url
+        }
+
+        func documentPickerWasCancelled(_ controller: UIDocumentPickerViewController) {
+            controller.dismiss(animated: true)
+        }
+    }
+}
+
 struct UploadUsdzView: View {
     
     var body: some View {

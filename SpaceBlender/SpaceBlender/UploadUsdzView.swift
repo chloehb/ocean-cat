be3371141c57d7375a0b741d7614c980c4acd29d
zyliang <zyliang@umich.edu>
2023-10-29 16:01:16 -0400

    fix code

--- SpaceBlender/SpaceBlender/UploadUsdzView.swift
+++ SpaceBlender/SpaceBlender/UploadUsdzView.swift
@@ -13,41 +13,6 @@ import MobileCoreServices
 
 import UniformTypeIdentifiers
 
-struct DocumentPicker: UIViewControllerRepresentable {
-    @Binding var fileURL: URL?
-    
-    func makeCoordinator() -> Coordinator {
-        Coordinator(self)
-    }
-    
-    func makeUIViewController(context: UIViewControllerRepresentableContext<DocumentPicker>) -> UIDocumentPickerViewController {
-        //        let documentPicker = UIDocumentPickerViewController(documentTypes: [String(kUTTypeText)], in: .import)
-        let documentPicker = UIDocumentPickerViewController(forOpeningContentTypes: [UTType.usdz], asCopy: true)
-        documentPicker.allowsMultipleSelection = false
-        documentPicker.delegate = context.coordinator
-        return documentPicker
-    }
-    
-    func updateUIViewController(_ uiViewController: UIDocumentPickerViewController, context: UIViewControllerRepresentableContext<DocumentPicker>) {}
-    
-    class Coordinator: NSObject, UIDocumentPickerDelegate {
-        var parent: DocumentPicker
-        
-        init(_ pickerController: DocumentPicker) {
-            self.parent = pickerController
-        }
-        
-        func documentPicker(_ controller: UIDocumentPickerViewController, didPickDocumentsAt urls: [URL]) {
-            guard let url = urls.first else { return }
-            parent.fileURL = url
-        }
-        
-        func documentPickerWasCancelled(_ controller: UIDocumentPickerViewController) {
-            controller.dismiss(animated: true)
-        }
-    }
-}
-
 struct UploadUsdzView: View {
     @State private var fileURL: URL?
     @State private var showDocumentPicker = false
@@ -78,9 +43,6 @@ struct UploadUsdzView: View {
                     print("File picker error: \(error.localizedDescription)")
                 }
             }
-        }
-        .sheet(isPresented: $showDocumentPicker) {
-            DocumentPicker(fileURL: $fileURL)
         }
     }
 }

74d19dd5327f4d54fb24f42d6c54089796085502
akaiDing <tingtind@umich.edu>
2023-11-01 19:46:32 -0400

    integrate buttons

--- SpaceBlender/SpaceBlender/UploadFileView.swift
+++ SpaceBlender/SpaceBlender/UploadFileView.swift
@@ -7,10 +7,27 @@
 
 import Foundation
 import SwiftUI
+import UniformTypeIdentifiers
+import RoomPlan
 
+func loadCapturedRoomFromJSONFile(filePath: URL) -> CapturedRoom? {
+    do {
+        let jsonData = try Data(contentsOf: filePath)
+        let decoder = JSONDecoder()
+        let capturedRoom = try decoder.decode(CapturedRoom.self, from: jsonData)
+        
+        return capturedRoom
+    } catch {
+        print("Error loading JSON file: \(error)")
+        return nil
+    }
+}
 struct UploadFileView: View {
     @Binding var isPresented: Bool
     @ObservedObject var store = ModelStore.shared
+    @Environment(\.dismiss) private var dismiss
+    
+    @State private var showDocumentPicker = false
     var body: some View {
 //        HStack {
 //            Button {
@@ -39,7 +56,7 @@ struct UploadFileView: View {
                     .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fit/*@END_MENU_TOKEN@*/)
                     .padding(.all)
                 Button {
-                    //isPresentingSelectMethod.toggle()
+                    showDocumentPicker.toggle()
                 } label: {
                     Text("Upload File")
                         .padding()
@@ -52,6 +69,32 @@ struct UploadFileView: View {
                 .cornerRadius(20)
                 .shadow(color: .blue, radius: 3, y: 3)
                 .padding()
+            }.fileImporter(isPresented: $showDocumentPicker, allowedContentTypes: [UTType.json], allowsMultipleSelection: false) {
+                result in
+                switch result {
+                case .success(let urls):
+                    
+                    if let url = urls.first {
+                        // Securely access the URL to save a bookmark
+                        guard url.startAccessingSecurityScopedResource() else {
+                            // Handle the failure here.
+                            print("fail to get access to \(url)")
+                            return
+                        }
+                        
+                        // We have to stop accessing the resource no matter what
+                        defer { url.stopAccessingSecurityScopedResource() }
+                        //                        print("Selected URLs: \(url)")
+                        let fileName = url.lastPathComponent
+                        let addCapturedRoom = loadCapturedRoomFromJSONFile(filePath: url)
+                        store.addNewModel(RoomModel(identifier: addCapturedRoom?.identifier, name: fileName, model: addCapturedRoom, date: Date().formatted()))
+                        dismiss() // todo: after upload file, go back to the home page or room gallery page
+                    } else {
+                        print("Empty URLS")
+                    }
+                case .failure(let error):
+                    print("File picker error: \(error.localizedDescription)")
+                }
             }
         }
 
